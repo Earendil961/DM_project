682b83631d783b6fe7f61e192d7c0ada55e171e9
Earendil961 <143197091+Earendil961@users.noreply.github.com>
2025-05-24 18:31:23 +0300

    Logic_name_update

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def create_gk(x, n, k):
             gk[neighbor][i] = True
     return gk
 
-def max_degree(n, gd):
+def size_max_independent_set(n, gd):
     arr = {}
     for i in range(n):
       arr[i] = []
@@ -42,7 +42,7 @@ def max_degree(n, gd):
     
     return len(ans)
 
-def size_max_independent_set(n, gk):
+def max_degree(n, gk):
     arr = np.zeros(n)
     for i in range(n):
       for j in range(n):
@@ -54,7 +54,7 @@ def size_max_independent_set(n, gk):
         m = arr[i]
     return m
 
-def td_2(adj_matrix):
+def size_max_clique(adj_matrix):
     n = len(adj_matrix)
     max_clique = 0
     for i in range(n):
@@ -69,7 +69,7 @@ def td_2(adj_matrix):
             max_clique = current_clique
     return max_clique
 
-def t_k_2(data):
+def number_of_connectivity_components(data):
     n = len(data)
     if n == 0:
         return 0
@@ -91,25 +91,25 @@ def t_k_2(data):
     return count
 
 
-def first_analyze(param_range, n, input_k_or_d, type_analyze):
+def Analyze_of_parametrs(param_range, n, input_k_or_d, type_analyze):
     t_values = []
     for param in param_range:
-        if type_analyze == 'knn':
+        if type_analyze == 'stud':
             samples = np.random.standard_t(df=param, size=n)
             graph = create_gk(samples.reshape(-1, 1), n, input_k_or_d)
-            t_val = size_max_independent_set(n, graph)
-        elif type_analyze == 'd':
+            t_val = max_degree(n, graph)
+        elif type_analyze == 'lap':
             samples = np.random.laplace(loc=0, scale=param, size=n)
             graph = create_gd(samples, n, input_k_or_d)
-            t_val = max_degree(n, graph)
-        elif type_analyze == 'knn_2':
+            t_val = size_max_independent_set(n, graph)
+        elif type_analyze == 'weib':
             samples = np.random.weibull(a=1/2, size=n) * param
             graph = create_gk(samples.reshape(-1, 1), n, input_k_or_d)
-            t_val = t_k_2(graph)
+            t_val = number_of_connectivity_components(graph)
         else:
             samples = np.random.exponential(scale=1/param, size=n)
             graph = create_gd(samples, n, input_k_or_d)
-            t_val = td_2(graph)
+            t_val = size_max_clique(graph)
         t_values.append(t_val)
 
     plt.figure()
@@ -119,29 +119,30 @@ def first_analyze(param_range, n, input_k_or_d, type_analyze):
     plt.grid()
     plt.show()
 
+
 def Analyze_for_k_and_d(par_1_or_2, n, input_k_or_d_mas, type_analyze):
     t_values = []
-    if type_analyze == 'knn':
+    if type_analyze == 'stud':
       samples = np.random.standard_t(df=par_1_or_2, size=n)
-    elif type_analyze == 'd':
+    elif type_analyze == 'lap':
       samples = np.random.laplace(loc=0, scale=par_1_or_2, size=n)
-    elif type_analyze == 'knn_2':
+    elif type_analyze == 'weib':
       samples = np.random.weibull(a=1/2, size=n) * par_1_or_2
     else:
       samples = np.random.exponential(scale=1/par_1_or_2, size=n)
     for input_k_or_d in input_k_or_d_mas:
-        if type_analyze == 'knn':
+        if type_analyze == 'stud':
             graph = create_gk(samples.reshape(-1, 1), n, input_k_or_d)
-            t_val = size_max_independent_set(n, graph)
-        elif type_analyze == 'd':
-            graph = create_gd(samples, n, input_k_or_d)
             t_val = max_degree(n, graph)
-        elif type_analyze == 'knn_2':
+        elif type_analyze == 'lap':
+            graph = create_gd(samples, n, input_k_or_d)
+            t_val = size_max_independent_set(n, graph)
+        elif type_analyze == 'weib':
             graph = create_gk(samples.reshape(-1, 1), n, input_k_or_d)
-            t_val = t_k_2(graph)
+            t_val = number_of_connectivity_components(graph)
         else:
             graph = create_gd(samples, n, input_k_or_d)
-            t_val = td_2(graph)
+            t_val = size_max_clique(graph)
         t_values.append(t_val)
 
     plt.figure()
@@ -151,25 +152,25 @@ def Analyze_for_k_and_d(par_1_or_2, n, input_k_or_d_mas, type_analyze):
     plt.grid()
     plt.show()
 
-def Analyze_for_n(par_1_or_2, n, type_analyze, n_range, input_k_or_d1):
+def Analyze_of_n(par_1_or_2, n, type_analyze, n_range, input_k_or_d1):
     t_values1 = []
     for n1 in n_range:
         if type_analyze == 'stud':
             samples1 = np.random.standard_t(df=par_1_or_2, size=n1)
             graph1 = create_gk(samples1.reshape(-1, 1), n1, input_k_or_d1)
-            t_val = size_max_independent_set(n1, graph1)
+            t_val = max_degree(n1, graph1)
         elif type_analyze == 'lap':
             samples1 = np.random.laplace(loc=0, scale=par_1_or_2, size=n1)
             graph1 = create_gd(samples1, n1, input_k_or_d1)
-            t_val = max_degree(n1, graph1)
+            t_val = size_max_independent_set(n1, graph1)
         elif type_analyze == 'weib':
             samples1 = np.random.weibull(a=1/2, size=n1) * par_1_or_2
             graph1 = create_gk(samples1.reshape(-1, 1), n1, input_k_or_d1)
-            t_val = t_k_2(graph1)
+            t_val = number_of_connectivity_components(graph1)
         else:
             samples1 = np.random.exponential(scale=1/par_1_or_2, size=n1)
             graph1 = create_gd(samples1, n1, input_k_or_d1)
-            t_val = td_2(graph1)
+            t_val = size_max_clique(graph1)
         t_values1.append(t_val)
 
     plt.figure()
@@ -190,39 +191,45 @@ if __name__ == "__main__":
   input_k = int(input("Введите k: "))
   input_d = float(input("Введите d: "))
 
-# =========== 1 ======================
+  # =========== 1 ======================
   n = 100
   print("Анализ четырех функций по их параметрам")
   param_range = np.linspace(2, 30, 100)
-  first_analyze(param_range, n, input_k, 'knn')
+  Analyze_of_parametrs(param_range, n, input_k, 'stud')
 
   param_range = np.linspace(0.5, 10, 70)
-  first_analyze(param_range, n, input_d, 'd')
+  Analyze_of_parametrs(param_range, n, input_d, 'lap')
 
   param_range = np.linspace(2, 30, 45)
-  first_analyze(param_range, n, input_k, 'knn_2')
+  Analyze_of_parametrs(param_range, n, input_k, 'weib')
 
   param_range = np.linspace(0.5, 10, 60)
-  first_analyze(param_range, n, input_d, 'd_2')
+  Analyze_of_parametrs(param_range, n, input_d, 'exp')
 
-# =========== 2 ======================
+  # =========== 2 ======================
   print("Анализ четырех функций по k, d и n")
+  print("1) stud")
   n_range = range(50, 500, 50)
   k_range = range(2, 20)
-  Analyze_for_k_and_d(par_1, n, k_range, 'knn')
-  Analyze_for_n(par_1, n, 'stud', n_range, input_k)
+  Analyze_for_k_and_d(par_1, n, k_range, 'stud')
+  Analyze_of_n(par_1, n, 'stud', n_range, input_k)
 
+  print("2) lap")
   d_range = np.linspace(0.05, 10, 30)
-  Analyze_for_k_and_d(par_2, n, d_range, 'd')
-  Analyze_for_n(par_2, n, 'lap', n_range, input_d)
-
+  Analyze_for_k_and_d(par_2, n, d_range, 'lap')
+  Analyze_of_n(par_2, n, 'lap', n_range, input_d)
+ 
+  print("3) weib")
   n_range = range(50, 100, 5)
   k_range = range(2, 20)
-  Analyze_for_k_and_d(par_3, n, k_range, 'knn_2')
-  Analyze_for_n(par_3, n, 'weib', n_range, input_k)
+  Analyze_for_k_and_d(par_3, n, k_range, 'weib')
+  Analyze_of_n(par_3, n, 'weib', n_range, input_k)
 
+  print("4) exp")
   d_range = np.linspace(0.05, 10, 30)
   Analyze_for_k_and_d(par_4, n, d_range, 'exp')
-  Analyze_for_n(par_4, n, 'exp', n_range, input_d)
+  Analyze_of_n(par_4, n, 'exp', n_range, input_d)
   # =========== 3 ======================
-  print("3_part")
+  print("Построение множества А")
+
+  # ============= Часть 2 =================
