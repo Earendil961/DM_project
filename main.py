f703b8eb4f2b61351b7e15c24418dfda513b2fed
Earendil961 <143197091+Earendil961@users.noreply.github.com>
2025-05-24 18:01:52 +0300

    Update_of_names_and_function_n

--- main.py
+++ main.py
@@ -22,7 +22,7 @@ def create_gk(x, n, k):
             gk[neighbor][i] = True
     return gk
 
-def t_d(n, gd):
+def max_degree(n, gd):
     arr = {}
     for i in range(n):
       arr[i] = []
@@ -42,7 +42,7 @@ def t_d(n, gd):
     
     return len(ans)
 
-def t_k(n, gk):
+def size_max_independent_set(n, gk):
     arr = np.zeros(n)
     for i in range(n):
       for j in range(n):
@@ -97,14 +97,14 @@ def first_analyze(param_range, n, input_k_or_d, type_analyze):
         if type_analyze == 'knn':
             samples = np.random.standard_t(df=param, size=n)
             graph = create_gk(samples.reshape(-1, 1), n, input_k_or_d)
-            t_val = t_k(n, graph)
+            t_val = size_max_independent_set(n, graph)
         elif type_analyze == 'd':
             samples = np.random.laplace(loc=0, scale=param, size=n)
             graph = create_gd(samples, n, input_k_or_d)
-            t_val = t_d(n, graph)
+            t_val = max_degree(n, graph)
         elif type_analyze == 'knn_2':
             samples = np.random.weibull(a=1/2, size=n) * param
-            graph = create_gd(samples, n, input_k_or_d)
+            graph = create_gk(samples.reshape(-1, 1), n, input_k_or_d)
             t_val = t_k_2(graph)
         else:
             samples = np.random.exponential(scale=1/param, size=n)
@@ -114,12 +114,12 @@ def first_analyze(param_range, n, input_k_or_d, type_analyze):
 
     plt.figure()
     plt.plot(param_range, t_values)
-    plt.xlabel('Parameter value')
+    plt.xlabel('Parameter k or d value')
     plt.ylabel(f'T value')
     plt.grid()
     plt.show()
 
-def second_analyze(par_1_or_2, n, input_k_or_d_mas, type_analyze):
+def Analyze_for_k_and_d(par_1_or_2, n, input_k_or_d_mas, type_analyze):
     t_values = []
     if type_analyze == 'knn':
       samples = np.random.standard_t(df=par_1_or_2, size=n)
@@ -132,12 +132,12 @@ def second_analyze(par_1_or_2, n, input_k_or_d_mas, type_analyze):
     for input_k_or_d in input_k_or_d_mas:
         if type_analyze == 'knn':
             graph = create_gk(samples.reshape(-1, 1), n, input_k_or_d)
-            t_val = t_k(n, graph)
+            t_val = size_max_independent_set(n, graph)
         elif type_analyze == 'd':
             graph = create_gd(samples, n, input_k_or_d)
-            t_val = t_d(n, graph)
+            t_val = max_degree(n, graph)
         elif type_analyze == 'knn_2':
-            graph = create_gd(samples, n, input_k_or_d)
+            graph = create_gk(samples.reshape(-1, 1), n, input_k_or_d)
             t_val = t_k_2(graph)
         else:
             graph = create_gd(samples, n, input_k_or_d)
@@ -151,52 +151,78 @@ def second_analyze(par_1_or_2, n, input_k_or_d_mas, type_analyze):
     plt.grid()
     plt.show()
 
+def Analyze_for_n(par_1_or_2, n, type_analyze, n_range, input_k_or_d1):
+    t_values1 = []
+    for n1 in n_range:
+        if type_analyze == 'stud':
+            samples1 = np.random.standard_t(df=par_1_or_2, size=n1)
+            graph1 = create_gk(samples1.reshape(-1, 1), n1, input_k_or_d1)
+            t_val = size_max_independent_set(n1, graph1)
+        elif type_analyze == 'lap':
+            samples1 = np.random.laplace(loc=0, scale=par_1_or_2, size=n1)
+            graph1 = create_gd(samples1, n1, input_k_or_d1)
+            t_val = max_degree(n1, graph1)
+        elif type_analyze == 'weib':
+            samples1 = np.random.weibull(a=1/2, size=n1) * par_1_or_2
+            graph1 = create_gk(samples1.reshape(-1, 1), n1, input_k_or_d1)
+            t_val = t_k_2(graph1)
+        else:
+            samples1 = np.random.exponential(scale=1/par_1_or_2, size=n1)
+            graph1 = create_gd(samples1, n1, input_k_or_d1)
+            t_val = td_2(graph1)
+        t_values1.append(t_val)
+
+    plt.figure()
+    plt.plot(n_range, t_values1)
+    plt.xlabel('N value')
+    plt.ylabel(f'T value')
+    plt.grid()
+    plt.show()
+
 
 if __name__ == "__main__":
-  '''
-  par_1 = float(input("Введите первый параметр распределения: "))
-  par_2 = float(input("Введите второй параметр распределения: "))
-  par_3 = float(input("Введите третий параметр распределения: "))
-  par_4 = float(input("Введите четвертый параметр распределения: "))
-  '''
+
   par_1 = 3
   par_2 = 0.70710678118
   par_3 = 0.31622776601
   par_4 = 1
-  E_mas = []
-  input_n = int(input("Введите n: "))
 
   input_k = int(input("Введите k: "))
   input_d = float(input("Введите d: "))
-# =========== 1 ====================
 
-# =========== 2 ======================
+# =========== 1 ======================
   n = 100
-  print("First_part")
+  print("Анализ четырех функций по их параметрам")
   param_range = np.linspace(2, 30, 100)
   first_analyze(param_range, n, input_k, 'knn')
 
   param_range = np.linspace(0.5, 10, 70)
   first_analyze(param_range, n, input_d, 'd')
 
-  param_range = np.linspace(2, 30, 50)
+  param_range = np.linspace(2, 30, 45)
   first_analyze(param_range, n, input_k, 'knn_2')
 
-  param_range = np.linspace(0.5, 10, 70)
+  param_range = np.linspace(0.5, 10, 60)
   first_analyze(param_range, n, input_d, 'd_2')
 
-# =========== 3 ======================
-  print("Second_part")
+# =========== 2 ======================
+  print("Анализ четырех функций по k, d и n")
+  n_range = range(50, 500, 50)
   k_range = range(2, 20)
-  second_analyze(par_1, n, k_range, 'knn')
+  Analyze_for_k_and_d(par_1, n, k_range, 'knn')
+  Analyze_for_n(par_1, n, 'stud', n_range, input_k)
 
   d_range = np.linspace(0.05, 10, 30)
-  second_analyze(par_2, n, d_range, 'd')
+  Analyze_for_k_and_d(par_2, n, d_range, 'd')
+  Analyze_for_n(par_2, n, 'lap', n_range, input_d)
 
+  n_range = range(50, 100, 5)
   k_range = range(2, 20)
-  second_analyze(par_1, n, k_range, 'knn_2')
+  Analyze_for_k_and_d(par_3, n, k_range, 'knn_2')
+  Analyze_for_n(par_3, n, 'weib', n_range, input_k)
 
   d_range = np.linspace(0.05, 10, 30)
-  second_analyze(par_2, n, d_range, 'd_2')
-  # =========== 4 ======================
+  Analyze_for_k_and_d(par_4, n, d_range, 'exp')
+  Analyze_for_n(par_4, n, 'exp', n_range, input_d)
+  # =========== 3 ======================
   print("3_part")
