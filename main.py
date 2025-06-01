374010f7521ae7c7c7749858950cf1204e562230
Rezomdy <dildinilya@gmail.com>
2025-06-01 18:23:52 +0300

    Багфикс 5

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report
+from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier, NearestNeighbors
 
 
