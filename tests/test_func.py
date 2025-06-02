4b572feab8b2b1ec40032c1be64e573f03b678d1
Rezomdy <dildinilya@gmail.com>
2025-06-02 20:40:41 +0300

    Добавление теста-заглушки

--- tests/test_func.py
+++ tests/test_func.py
@@ -1,1 +1,10 @@
-pass
+import unittest
+
+
+class TestStub(unittest.TestCase):
+    def test_always_pass(self):
+        self.assertTrue(True, "Always pass")
+
+
+if __name__ == '__main__':
+    unittest.main()
