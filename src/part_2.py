eb7d0d568a41b2bc5b9bdc3555715245ff3fb9cd
Earendil961 <143197091+Earendil961@users.noreply.github.com>
2025-06-03 21:39:25 +0300

    Update part_2.py

--- src/part_2.py
+++ src/part_2.py
@@ -3,7 +3,10 @@ import numpy as np
 import pandas as pd
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, classification_report
+from sklearn.metrics import (
+    accuracy_score,
+    classification_report,
+)
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
 
