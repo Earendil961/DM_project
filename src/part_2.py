6e1a2b2eae4cf869e829165d16680e4d96480560
Earendil961 <143197091+Earendil961@users.noreply.github.com>
2025-06-03 21:36:53 +0300

    Update part_2.py

--- src/part_2.py
+++ src/part_2.py
@@ -6,9 +6,14 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, classification_report
 from sklearn.model_selection import train_test_split
 from sklearn.neighbors import KNeighborsClassifier
-from part_1 import (create_gd, max_degree,
-                        number_of_connectivity_components, size_max_clique,
-                        size_max_independent_set)
+
+from part_1 import (
+    create_gd,
+    max_degree,
+    number_of_connectivity_components,
+    size_max_clique,
+    size_max_independent_set,
+)
 
 
 def extract_multiple_features(samples, n, k_or_d, graph_type):
